78de7f77d43a2e516061744aa472177382a93640
nyantama <r_nyantama0616@yahoo.co.jp>
2023-10-23 22:41:27 +0900

    GET /users/:id を実装 その2
    Basic認証ありの実装をする
    
    手順
    1. controllerを適切に編集
    
    何も問題がなければ、例えば、GET /users/1 を送ると、id=1のユーザの情報が、レスポンスとして返ってくる。
    
    ただし、リクエストの際、headerに認証情報を含まなければならない。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
+include ActionController::HttpAuthentication::Basic::ControllerMethods #authenticate_or_request_with_http_basicメソッドがこのモジュールに含まれてるから、includeする
+
 class UsersController < ApplicationController
+    before_action :basic_auth, only: [:show] #showアクション実行時のみ、Basic認証を要求する
+    
     def show
-        user = User.find(params["id"])
-        render json: {user: user}
+        # Basic認証ではブラウザに認証情報を保存する。
+        # 認証が必要な処理の場合は、リクエスト時に認証情報をサーバに送り、都度その認証情報が正しいか検証する。
+        # 下記では便宜上、認証情報をレスポンスに含んでいるが、本来は認証情報を送り返すなんてことはしないはず。てかやっちゃダメ。
+        auth = request.headers["Authorization"] #これが認証情報！
+        render json: {user: @current_user, auth: auth}
     end
 
     def create
@@ -11,6 +18,15 @@ class UsersController < ApplicationController
         else
             errors = user.errors.full_messages
             render json: {message: "Failed...", errors: errors} #ユーザ作成に失敗した場合は、エラーメッセージを返す
+        end
+    end
+
+    private
+
+    def basic_auth
+        authenticate_or_request_with_http_basic do |user_id, password|
+            @current_user = User.find_by(user_id: user_id, password: password) #インスタンス変数に代入することで、users_controller内のどこからでも、@current_userを参照できる
+            !!@current_user #true or false
         end
     end
 end
