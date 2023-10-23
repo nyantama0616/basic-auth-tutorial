ad222ba5ed2fff958216ce1fdbda4949642d1b3d
nyantama <r_nyantama0616@yahoo.co.jp>
2023-10-23 23:19:47 +0900

    POST /close の実装
    ユーザを削除できるようにする。
    
    手順
    1. config/routes.rbでルーティング設定
    2. controllerを適切に編集
    
    何も問題がなければ、POST /closeを送ると、ユーザを削除することができる。
    
    ただし、リクエストの際、headerに認証情報を含まなければならない。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 include ActionController::HttpAuthentication::Basic::ControllerMethods #authenticate_or_request_with_http_basicメソッドがこのモジュールに含まれてるから、includeする
 
 class UsersController < ApplicationController
-    before_action :basic_auth, only: [:show, :update] #show, updateアクション実行時のみ、Basic認証を要求する
+    before_action :basic_auth, only: [:show, :update, :destroy] #show, update, destroyアクション実行時のみ、Basic認証を要求する
 
     def index
         users = User.all
@@ -29,6 +29,12 @@ class UsersController < ApplicationController
     def update
         @current_user.update(nickname: params["nickname"], comment: params["comment"])
         render json: {user: @current_user}
+    end
+
+    def destroy
+        @current_user.destroy
+        users = User.all
+        render json: {users: users} #ちゃんとユーザが削除されたか確認するため、ユーザ一覧を返している
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   get "users/:id", to: "users#show"
   get "users", to: "users#index"
   patch "users/:id", to: "users#update"
+  post "close", to: "users#destroy"
 end
